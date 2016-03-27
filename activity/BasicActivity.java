83680ead954a0ccef3fb23be730a096f7bb60815
““cwind” <1441955828@qq.com>
2016-03-27 20:03:57 +0800

    change in initwindows

--- activity/BasicActivity.java
+++ activity/BasicActivity.java
@@ -1,7 +1,12 @@
 package com.app.carrot.smartmanager.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class BasicActivity extends Activity {
 
@@ -10,7 +15,27 @@ public class BasicActivity extends Activity {
         super.onCreate(savedInstanceState);
 
     }
-    public void initWindows(){
+    public void initWindows(Activity activity, View titleViewGroup) {
+        if (activity == null)
+            return;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = activity.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        if (titleViewGroup == null)
+            return;
+        // 设置头部控件ViewGroup的PaddingTop,防止界面与状态栏重叠
+        int statusBarHeight = getStatusBarHeight(activity);
+        titleViewGroup.setPadding(0, statusBarHeight, 0, 0);
+        }
 
+    private static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
